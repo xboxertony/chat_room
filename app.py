96c3efab78c647f8efe5f4829fa68e9187b7fc10
Tony Tsai <tonyny58@gmail.com>
2021-06-19 00:07:14 +0800

    chat

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,session,request,redirect,url_for
+from flask_socketio import SocketIO,emit,join_room,leave_room
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+socketio = SocketIO(app)
+
+@app.route("/chat")
+def chat():
+    room = session["room"]
+    return render_template("index.html",room = room)
+
+@socketio.on("my event",namespace="/chat")
+def index(data):
+    join_room(session.get("room"))
+    # emit("status",data)
+
+@socketio.on("text",namespace="/chat")
+def get_msg(data):
+    room = session["room"]
+    emit("message",session["user"]+" : "+data,room = room)
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    if request.method=="POST" and request.form["user"] and request.form["room"]:
+        session["user"]=request.form["user"]
+        session["room"]=request.form["room"]
+        return redirect(url_for("chat"))
+    return render_template("home.html")
+
+@socketio.on("left",namespace="/chat")
+def leave(data):
+    room = session["room"]
+    leave_room(session["room"])
+    print(data)
+    emit("message",session["user"]+" leave the room \n",room = room)
+
+@socketio.on("broadcast",namespace="/chat")
+def b(data):
+    emit("message",session["user"]+" from "+session["room"]+"  "+data+" \n",broadcast=True)
+
+if __name__=="__main__":
+    app.debug = True
+    socketio.run(app)
