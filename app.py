869fcc6d3cb1013b50728c764699cb92b2bb240d
Tony Tsai <tonyny58@gmail.com>
2022-03-07 22:58:25 +0800

    add http emit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from time import sleep
 from flask import Flask,render_template,session,request,redirect,url_for
 from flask_socketio import SocketIO,emit,join_room,leave_room
 
@@ -18,7 +19,20 @@ def index(data):
 @socketio.on("text",namespace="/chat")
 def get_msg(data):
     room = session["room"]
+    print(request.sid)
     emit("message",session["user"]+" : "+data,room = room)
+    sleep(10)
+    emit("message","I am Here",room = room)
+
+@app.route("/jjj")
+def jjj():
+    socketio.emit("message1",{"data":"Are you ok?"},namespace='/chat',to=session['room'])
+    return "ok"
+
+@app.route("/ggg")
+def ggg():
+    socketio.emit("message1",{"data":"Are you ok?"},namespace='/chat',to=session['room'])
+    return "ok"
 
 @app.route("/",methods=["GET","POST"])
 def home():
